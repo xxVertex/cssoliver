ba338b1df9602bf8de3369a63354ceb785bc21e2
Oliver <oliverkentsantos@outlook.es>
2025-12-02 10:29:22 +0100

    fixed build system

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
     plugins: [
-        require("postcss-import"),
-        require("autoprefixer"),
-        require("cssnano")({
-            preset: "default",
-        }),
-    ],
-};  
+      require('postcss-import'),
+      require('autoprefixer'),
+      ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
+    ]
+};
